9d3d1d9b84e7cc5fc206aa983e2bab5579e6c102
Oscar Davies <oscar.william.davies@outlook.com>
2019-11-29 00:38:24 +0000

    Speed up AA by skipping blank pixles

--- DataVisulisation/BitMapManipulation.cs
+++ DataVisulisation/BitMapManipulation.cs
@@ -11,7 +11,8 @@ namespace DataVisulisation
             Bitmap BMPOriginal = (Bitmap)Bmp.Clone();
             for (int x = 0, y = 0; y < Bmp.Height; x++)
             {
-                Bmp.SetPixel(x, y, BitMapAnalysis.PixelAverage(BMPOriginal, new Point(x, y), 1, CenterMultiplyer));
+                if (!Color.FromArgb(0,0,0,0).Equals(Bmp.GetPixel(x,y)))
+                { Bmp.SetPixel(x, y, BitMapAnalysis.PixelAverage(BMPOriginal, new Point(x, y), 1, CenterMultiplyer)); }
                 if (x == Bmp.Width - 1) { x = -1; y++; }
             }
             BMPOriginal.Dispose();
